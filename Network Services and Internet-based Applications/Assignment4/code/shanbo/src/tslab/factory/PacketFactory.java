35adaa3e9533e4d2c0af931da8a861adca75096a
shanboli@gmail.com <shanboli@gmail.com@614f7409-4139-0410-a961-afbe15764031>
2008-05-17 19:29:09 +0000

    just a backup

---
+++ Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/factory/PacketFactory.java
@@ -0,0 +1,11 @@
+package tslab.factory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Develop with pleasure.
+ * User: Shanbo Li
+ * Date: May 17, 2008
+ * Time: 11:24:14 PM
+ */
+public abstract class PacketFactory {
+}
