9783e78ea3127157d4c62e4e366cca53fbe0e213
shanboli@gmail.com <shanboli@gmail.com@614f7409-4139-0410-a961-afbe15764031>
2008-05-18 09:31:20 +0000

    just a backup

--- trunk/Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/factory/ICMPFactory.java
+++ trunk/Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/factory/ICMPFactory.java
@@ -2,11 +2,14 @@ package tslab.factory;
 
 import jpcap.packet.ICMPPacket;
 import jpcap.packet.IPPacket;
+import jpcap.packet.EthernetPacket;
 import tslab.exception.WrongInputPacketException;
 import tslab.util.ICMPSessionMapping;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,42 +20,71 @@ import java.util.ArrayList;
  */
 public class ICMPFactory implements PacketFactory {
 
-    List <ICMPSessionMapping> sessions = new ArrayList<ICMPSessionMapping>();
+    List<ICMPSessionMapping> sessions = new ArrayList<ICMPSessionMapping>();
+    private InetAddress serverAddress;
+    private byte[] serverMac;
 
     private final static ICMPFactory instance = new ICMPFactory();
 
-    private ICMPFactory (){
+    private ICMPFactory() {
 
     }
 
-    public static ICMPFactory getInstance(){
+    /**
+     * Get a instance of ICMPFactory, pass serverAddress and serverMac to it
+     *
+     * @param serverAddress
+     * @param serverMac
+     * @return
+     */
+    public static ICMPFactory getInstance(String serverAddress, byte[] serverMac) throws UnknownHostException {
+        if (instance.serverAddress == null && instance.serverMac == null) {
+            instance.serverAddress = InetAddress.getByName(serverAddress);
+            instance.serverMac = serverMac;
+        }
         return instance;
     }
 
     /**
      * Produce a packet <b>to server</b> according the incoming packet from client.
+     *
      * @param ipPacket the packet which comes from client
      * @return a packet which will be send <b>to server</b> according the incoming packet from client.
      * @throws WrongInputPacketException incoming packet is not a ICMP packet.
      */
     public IPPacket toServer(IPPacket ipPacket) throws WrongInputPacketException {
-        ICMPPacket inPacket;
-        ICMPPacket outPacket = null;
-        if (ipPacket instanceof ICMPPacket){
-            inPacket = (ICMPPacket)ipPacket;
-        }   else {
+        ICMPPacket icmpIn;
+        ICMPPacket icmpOut = null;
+        if (ipPacket instanceof ICMPPacket) {
+            icmpIn = (ICMPPacket) ipPacket;
+        } else {
             throw new WrongInputPacketException("Not a ICMP packet");
         }
 
-        
+        //add packet to sesson mapping
+        sessions.add(new ICMPSessionMapping(icmpIn.src_ip, icmpIn.id, icmpIn.seq));
+
+        //produce packet to server
+        //TODO mac address to server is not correct here.
+        EthernetPacket ethIn = (EthernetPacket) icmpIn.datalink;
+        icmpOut.type = ICMPPacket.ICMP_ECHOREPLY; // 0
+        icmpOut.id = icmpIn.id;
+        icmpOut.seq = icmpIn.seq;
+        icmpOut.setIPv4Parameter(0, false, false, false, 0, false, false, false, 0, 1010101, 100, IPPacket.IPPROTO_ICMP, icmpIn.dst_ip, icmpIn.src_ip);
+        EthernetPacket ethOut = new EthernetPacket();
+        ethOut.frametype = EthernetPacket.ETHERTYPE_IP;
+        ethOut.src_mac = ethIn.dst_mac;
+        ethOut.dst_mac = ethIn.src_mac;
+        icmpOut.datalink = ethOut;
+        icmpOut.data = icmpIn.data;
 
 
-        
-        return outPacket;
+        return icmpOut;
     }
 
     /**
      * Produce a packet <b>to client</b> according the incoming packet from server.
+     *
      * @param ipPacket the packet which comes from server
      * @return a packet wich will be send <b>to client</b> according the incoming packet from server
      * @throws WrongInputPacketException incoming packet is not a ICMP packet.
@@ -60,14 +92,11 @@ public class ICMPFactory implements PacketFactory {
     public IPPacket toClient(IPPacket ipPacket) throws WrongInputPacketException {
         ICMPPacket inPacket;
         ICMPPacket outPacket = null;
-        if (ipPacket instanceof ICMPPacket){
-            inPacket = (ICMPPacket)ipPacket;
-        }   else {
+        if (ipPacket instanceof ICMPPacket) {
+            inPacket = (ICMPPacket) ipPacket;
+        } else {
             throw new WrongInputPacketException("Not a ICMP packet");
         }
-
-
-
 
 
         return outPacket;
