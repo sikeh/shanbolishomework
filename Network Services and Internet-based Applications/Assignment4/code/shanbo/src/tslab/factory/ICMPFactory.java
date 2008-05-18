c3b24b84a0c546ec70b34175dfe1f7fc0eb8a789
shanboli@gmail.com <shanboli@gmail.com@614f7409-4139-0410-a961-afbe15764031>
2008-05-18 09:07:52 +0000

    just a backup

--- Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/factory/ICMPFactory.java
+++ Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/factory/ICMPFactory.java
@@ -1,9 +1,12 @@
 package tslab.factory;
 
-import jpcap.packet.Packet;
 import jpcap.packet.ICMPPacket;
 import jpcap.packet.IPPacket;
 import tslab.exception.WrongInputPacketException;
+import tslab.util.ICMPSessionMapping;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,6 +17,24 @@ import tslab.exception.WrongInputPacketException;
  */
 public class ICMPFactory implements PacketFactory {
 
+    List <ICMPSessionMapping> sessions = new ArrayList<ICMPSessionMapping>();
+
+    private final static ICMPFactory instance = new ICMPFactory();
+
+    private ICMPFactory (){
+
+    }
+
+    public static ICMPFactory getInstance(){
+        return instance;
+    }
+
+    /**
+     * Produce a packet <b>to server</b> according the incoming packet from client.
+     * @param ipPacket the packet which comes from client
+     * @return a packet which will be send <b>to server</b> according the incoming packet from client.
+     * @throws WrongInputPacketException incoming packet is not a ICMP packet.
+     */
     public IPPacket toClient(IPPacket ipPacket) throws WrongInputPacketException {
         ICMPPacket inPacket;
         ICMPPacket outPacket = null;
@@ -22,11 +43,32 @@ public class ICMPFactory implements PacketFactory {
         }   else {
             throw new WrongInputPacketException("Not a ICMP packet");
         }
+
+        sessions.
+
+        
         return outPacket;
     }
 
+    /**
+     * Produce a packet <b>to client</b> according the incoming packet from server.
+     * @param ipPacket the packet which comes from server
+     * @return a packet wich will be send <b>to client</b> according the incoming packet from server
+     * @throws WrongInputPacketException
+     */
     public IPPacket toServer(IPPacket ipPacket) throws WrongInputPacketException {
-        //TODO: complete this method.
-        throw new UnsupportedOperationException("Not a ICMP packet");
+        ICMPPacket inPacket;
+        ICMPPacket outPacket = null;
+        if (ipPacket instanceof ICMPPacket){
+            inPacket = (ICMPPacket)ipPacket;
+        }   else {
+            throw new WrongInputPacketException("Not a ICMP packet");
+        }
+
+
+
+
+
+        return outPacket;
     }
 }
---
+++ Network Services and Internet-based Applications/Assignment4/code/shanbo/src/tslab/util/FTPSessionMapping.java
@@ -0,0 +1,12 @@
+package tslab.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Develop with pleasure.
+ * User: Shanbo Li
+ * Date: May 17, 2008
+ * Time: 11:03:36 PM
+ */
+public class FTPSessionMapping {
+    
+}
